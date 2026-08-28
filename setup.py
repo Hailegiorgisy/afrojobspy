eef0a5bd4b820f122f9c84487d0ba36128f611bb
Hailegiorgis Habtie <hgyirgu1@gmail.com>
2026-08-28 12:19:13 +0300

    Bump version to 1.0.0 and add license
    Updated version number to 1.0.0 and added license information.

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="afrojobspy",
-    version="0.1.0",
+    version="1.0.0",
+    license="MIT",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
